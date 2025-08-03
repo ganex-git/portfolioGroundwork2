0517efc38b43e91ce3552582426c166b2b57df19
ganex-git <aryanpeddi11@gmail.com>
2025-08-03 11:58:48 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 document.getElementsByTagName("h1")[0].style.fontSize = "6vw";
 
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add('fadeIn--visible');
-      entry.target.classList.remove('fadeIn--hidden');
-    } else {
-      entry.target.classList.remove('fadeIn--visible');
-      entry.target.classList.add('fadeIn--hidden');
-    }
-  });
-}, { threshold: 0.1 }); // tweak threshold to taste
+document.addEventListener("DOMContentLoaded", function () {
+  const faders = document.querySelectorAll('.fadeIn');
 
-document.querySelectorAll('.fadeIn').forEach(el => observer.observe(el));
+  const options = {
+    threshold: 0.1
+  };
+
+  const appearOnScroll = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('fadeIn--visible');
+        observer.unobserve(entry.target);
+      }
+    });
+  }, options);
+
+  faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+  });
+});
